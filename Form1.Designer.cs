2e68deb0b27a67b2594173e765cc6f8b30489065
Cael-Phillips <62359650+Cael-Phillips@users.noreply.github.com>
2020-08-09 16:37:38 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Lab4 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.DisplayBox = new System.Windows.Forms.RichTextBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.titleStrip = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.Load = new System.Windows.Forms.ToolStripMenuItem();
+            this.Process = new System.Windows.Forms.ToolStripMenuItem();
+            this.Check = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.titleStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DisplayBox
+            // 
+            this.DisplayBox.Location = new System.Drawing.Point(12, 85);
+            this.DisplayBox.Name = "DisplayBox";
+            this.DisplayBox.ReadOnly = true;
+            this.DisplayBox.Size = new System.Drawing.Size(382, 353);
+            this.DisplayBox.TabIndex = 0;
+            this.DisplayBox.Text = "";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Location = new System.Drawing.Point(13, 66);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(0, 13);
+            this.Title.TabIndex = 1;
+            // 
+            // titleStrip
+            // 
+            this.titleStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File,
+            this.Process,
+            this.Exit});
+            this.titleStrip.Location = new System.Drawing.Point(0, 0);
+            this.titleStrip.Name = "titleStrip";
+            this.titleStrip.Size = new System.Drawing.Size(406, 24);
+            this.titleStrip.TabIndex = 2;
+            this.titleStrip.Text = "menuStrip1";
+            // 
+            // File
+            // 
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Load});
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(37, 20);
+            this.File.Text = "File";
+            // 
+            // Load
+            // 
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(100, 22);
+            this.Load.Text = "Load";
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Process
+            // 
+            this.Process.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Check});
+            this.Process.Name = "Process";
+            this.Process.Size = new System.Drawing.Size(59, 20);
+            this.Process.Text = "Process";
+            // 
+            // Check
+            // 
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(133, 22);
+            this.Check.Text = "Check Tags";
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(38, 20);
+            this.Exit.Text = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 450);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.DisplayBox);
+            this.Controls.Add(this.titleStrip);
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.titleStrip.ResumeLayout(false);
+            this.titleStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox DisplayBox;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.MenuStrip titleStrip;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem Load;
+        private System.Windows.Forms.ToolStripMenuItem Process;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.ToolStripMenuItem Check;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab4 {
+    public partial class Form1 : Form {
+        private Stack<string> InText = new Stack<string>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e) {
+
+        }
+        
+        /// <summary>
+        /// When the Load button is clicked it will allow the user to select a file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Load_Click(object sender, EventArgs e) {
+            Stack<string> Lines = new Stack<string>();//Create a new Stack
+            var Line = string.Empty;//Create a variable that will receive info from the reader
+            //Create the OpenFileDialog
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Multiselect = false;
+            OFD.Filter = "HTML Files (*.html) | *.html";//Set the filter
+            OFD.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);//set the default path
+            //Read from the file
+            if (OFD.ShowDialog() == DialogResult.OK) {
+                var Stream = OFD.OpenFile();//Open the file
+                using (StreamReader reader =new StreamReader(Stream)) {//Initialize reader
+                    while ((Line = reader.ReadLine())!=null) {//Read line by line
+                        Lines.Push(Line);//add to Stack
+                    }//while
+                    while (Lines.Count != 0){
+                        InText.Push(Lines.Pop());//Reverse the Stack into a new one
+                    }//while
+                }//using
+            }//if
+        }//Load
+        
+        /// <summary>
+        /// Exits the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Exit_Click(object sender, EventArgs e) {
+            Environment.Exit(0);//Exit with code 0
+        }//Exit
+
+        /// <summary>
+        /// When the Check button is clicked it will check each line for opening and closing tags, while filtering non-containing tags
+        /// NOTE: Not all non-containing tags have been added. If this were being added to a piece of software that was being sold, I would add each tag.
+        /// I added the tags that were both listed in the assignment and any non-container tags in my test code
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Check_Click(object sender, EventArgs e) {
+            String OIndex = "<", CIndex = ">";//Identify the Index Characters of a tag
+            String [] Element, SplitBy = {"<",">"};//Identify what to split by, and make an array to hold elements of a line
+            String [] Ignore = { "img", "hr", "br", "link", "meta", "input","DOCTYPE" };//Identify the tags to ignore. SEE NOTE INT SUMMARY COMMENTS!!!
+            String [] Lines = InText.ToArray();//Convert Stack to an Array. I do this, so it's easier for me to traverse the lines of text. Alternatively I could have used InText.Pop() to select the line
+            int Open = 0, Close = 0, Size = Lines.Count();//Create variables to hold the number of open tags, close tags and the size of the array
+            bool Balanced = false;
+            var Skip = false;
+            var FinalText = string.Empty;
+            var Line=string.Empty;
+            //Create two counters
+            int x=0;
+            int y = 0;
+
+            //Breaking down to Elements
+            for (int i = 0; i < Size; i++){
+                Line = Lines[i].Trim();//Trim the Line
+                Element = Line.Split(SplitBy,StringSplitOptions.RemoveEmptyEntries);//Split the line
+                //Checking each line element
+                for (int j = 0; j < Element.Length; j++){
+                    Skip = false;
+                    x = 0;//Traverses the Ignore array
+                    y = 0;//Acts as an exit statement
+                    Element[j] = Element[j].Trim();//trim the element
+                    
+                    while (y<Ignore.Length){
+                        //If the selection matches a word that we need to Skip
+                        if (Element[j].Contains(Ignore[x]) == true) {
+                            //j++;//increment the selection counter
+                            Skip = true;//set Skip to true
+                            y++;
+                        }//if
+                        //If the selection does not matches a word we need to Skip
+                        else if (Element[j].Contains(Ignore[x]) == false) {
+                            x++;
+                            y++;
+                        }//elif
+                    }//while
+                    //If the word does not need to be skipped
+                    if (Skip != true){
+                        //if the Length is 0
+                        if (Lines[i].Length == 0) {
+                            break;
+                        }//if
+                        else{
+                            //Check to see if the first Character is a forward slash
+                            string Check = Element[j].Substring(0,1);
+                            if (Check.Equals("/")) {
+                                Close++;//Add a count to the closing tag count
+                                FinalText += "Found Closing Tag: <" + Element[j].ToString() + ">\n";//Add a line to the Final Text
+                            }//if
+                            else {
+                                Open++;//Add a count to the opening tag count
+                                FinalText += "Found Opening Tag: <" + Element[j].ToString() + ">\n";//Add a line to the Final Text
+                            }//else
+                        }//else
+                    }//else
+                }//for
+            }//for
+            //Check if the tags are balanced
+            if (Open == Close) {
+                Balanced = true;
+            }//if
+            else {
+                Balanced = false;
+            }//else
+            Update(Balanced,FinalText);//Call the update method
+        }//Check
+        /// <summary>
+        /// Updates the Text Box
+        /// </summary>
+        /// <param name="Balanced"></param>
+        /// <param name="FinalText"></param>
+        public void Update(bool Balanced, string FinalText){
+            //Check the Balanced condition
+            switch (Balanced){
+                case true://If it is balanced
+                    Title.Text = "The tags are balanced";
+                    break;
+                case false://If it is NOT balanced
+                    Title.Text = "The tags are not balanced";
+                    break;
+                default:
+                    break;
+            }//switch
+            DisplayBox.Text = FinalText;//Update the final text
+        }//Update
+    }//Form
+}//namespace
